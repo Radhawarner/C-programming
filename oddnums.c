fe175c7fd08e59387cf18cb134cbb2dc2cff5c36
Radhawarner <radhawarnar31@gmail.com>
2025-09-29 12:11:43 +0530

    Add files via upload

---
+++ oddnums.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	//method - 1
+//	for (int i = 1; i<=n; i++ ){
+//		if ( i % 2 == 1)
+//			printf("%d ", i);
+//	}
+	for (int i = 1; i<=n; i+=2)
+		printf("%d ", i);
+	return 0;
+}
