ee7d1c9e95995f87e014c078c7dfee6149d542ef
Radhawarner <radhawarnar31@gmail.com>
2025-09-29 11:59:29 +0530

    Add files via upload

---
+++ toggle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	char ch;
+	scanf("%c",&ch);
+	if ( ch >='A' && ch <='Z')
+		printf("Toggle case of '%c' is '%c'", ch,ch+32 );
+	else if (ch >='a' && ch <='z')
+		printf("Toggle case of '%c' is '%c'", ch,ch-32 );
+	else
+		printf("'%c' is not an alphabet", ch);
+	return 0;
+}
